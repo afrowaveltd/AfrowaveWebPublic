70256410dd494672aa0185a7d9736ff829857bfa
Bohuslav Pařenica <sparenica@outlook.com>
2025-01-06 22:19:06 +0100

    Login rules form done

---
+++ Id/Pages/Helps/Install_LoginRules_LockoutTime.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Id.Pages.Helps
+{
+    public class Install_LoginRules_LockoutTimeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Id/Pages/Helps/Install_LoginRules_MaxFailed.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Id.Pages.Helps
+{
+    public class Install_LoginRules_MaxFailedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
