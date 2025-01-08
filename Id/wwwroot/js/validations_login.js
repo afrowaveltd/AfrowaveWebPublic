ce1faa7b0e9385e5a9565d4d9bb86240efb4865c
Bohuslav Pařenica <sparenica@outlook.com>
2025-01-08 22:48:50 +0100

    form validation - part

--- Id/wwwroot/js/validations_login.js
+++ Id/wwwroot/js/validations_login.js
@@ -1,1 +1,77 @@
-﻿
+﻿const maxFailedError = document.getElementById('login_rules_err_max_failed');
+const lockoutTimeError = document.getElementById('login_rules_err_lockout_time');
+const passwordTokenError = document.getElementById('login_rules_err_password_reset_token_expiration');
+const emailTokenError = document.getElementById('login_rules_err_email_token_expiration');
+const requireConfirmation = document.getElementById('login_rules_err_require_email_confirmation');
+const submitButton = document.getElementById('submit');
+
+let maxFailedOk = true;
+let lockoutTimeOk = true;
+let passwordTokenOk = true;
+let emailTokenOk = true;
+let requireConfirmationOk = true;
+
+let warning = '';
+
+const checkForm = () => {
+	(maxFailedOk && lockoutTimeOk && passwordTokenOk && emailTokenOk && requireConfirmationOk)
+		? formOk = true
+		: formOk = false;
+	formOk
+		? submitButton.removeAttribute('disabled')
+		: submitButton.setAttribute('disabled', 'disabled');
+}
+
+const setValid = (element, errorElement) => {
+	element.classList.remove('input-invalid');
+	element.classList.add('input-valid');
+	errorElement.innerHTML = '';
+}
+const setInvalid = (element, errorElement, message) => {
+	element.classList.remove('input-valid');
+	element.classList.add('input-invalid');
+	errorElement.innerHTML = message;
+}
+
+const checkMaxFailed = async (element) => {
+	if (element.value < 0) {
+		warning = await localize("Max failed login attempts must be a positive number");
+		setInvalid(element, maxFailedError, warning);
+		maxFailedOk = false;
+	}
+	else if (element.value == 0) {
+		warning = await localize("Max failed login attempts set to 0 will disable account lockout");
+		setInvalid(element, maxFailedError, warning);
+		maxFailedOk = true;
+	}
+	else if (element.value < 3) {
+		warning = await localize("Max failed login attempts set to less than 3 is not recommended");
+		setInvalid(element, maxFailedError, warning);
+		maxFailedOk = true;
+	}
+	else {
+		warning = '';
+		setValid(element, maxFailedError);
+		maxFailedOk = true;
+	}
+	checkForm();
+}
+
+const checkLockoutTime = async (element) => {
+	if (element.value < 0) {
+		warning = await localize("Max lockout time must be a positive number");
+		setInvalid(element, lockoutTimeError, warning);
+		lockoutTimeOk = false;
+	}
+	else if (element.value == 0) {
+		warning = await localize("Attention! Account lockout time set to 0 will lock out accounts indefinitely");
+		setInvalid(element, lockoutTimeError, warning);
+		lockoutTimeOk = true;
+	}
+	else {
+		warning = '';
+		setValid(element, lockoutTimeError);
+		lockoutTimeOk = true;
+	}
+	checkForm();
+}
