4fe6d14f64bce8a98f07abe5882db7907a26e8d9
Bohuslav Pařenica <sparenica@outlook.com>
2025-01-15 15:37:36 +0100

    Installation result - first part

--- Id/Pages/Install/InstallationResult.cshtml.cs
+++ Id/Pages/Install/InstallationResult.cshtml.cs
@@ -1,12 +1,20 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace Id.Pages.Install
 {
-    public class InstallationResultModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class InstallationResultModel(IStringLocalizer<InstallationResultModel> _t,
+		ApplicationDbContext context,
+		ISettingsService settingsService,
+		IInstallationStatusService installationStatus,
+		ILogger<InstallationResultModel> logger) : PageModel
+	{
+		public readonly IStringLocalizer<InstallationResultModel> t = _t;
+		public readonly ApplicationDbContext _context = context;
+		public readonly ISettingsService _settingsService = settingsService;
+		public readonly IInstallationStatusService _statusService = installationStatus;
+		public readonly ILogger<InstallationResultModel> _logger = logger;
+
+		public async Task<IActionResult> OnGetAsync()
+		{
+			return Page();
+		}
+	}
 }
