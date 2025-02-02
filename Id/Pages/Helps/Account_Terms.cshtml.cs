d4d68a0bee443a0dcf640ac310574b6c7fb8b16c
Bohuslav Pařenica <sparenica@outlook.com>
2025-02-02 22:38:19 +0100

    Created Account Helpers empty razor pages

---
+++ Id/Pages/Helps/Account_Terms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Id.Pages.Helps
+{
+    public class Account_TermsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Id/Pages/Helps/Account_UserPicture.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Id.Pages.Helps
+{
+    public class Account_UserPictureModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
