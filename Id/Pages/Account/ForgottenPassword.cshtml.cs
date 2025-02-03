b213c1fb049303686a0c25e6069797577388fc72
Bohuslav Pařenica <sparenica@outlook.com>
2025-02-03 15:21:01 +0100

    translations added

---
+++ Id/Pages/Account/ForgottenPassword.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Id.Pages.Account
+{
+    public class ForgottenPasswordModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Id/Pages/Helps/Account_RegisterUser.cshtml.cs
+++ Id/Pages/Helps/Account_RegisterUser.cshtml.cs
@@ -1,12 +1,16 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
 namespace Id.Pages.Helps
 {
-    public class Account_RegisterUserModel : PageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
+	public class Account_RegisterUserModel(IStringLocalizer<Account_RegisterUserModel> _t) : PageModel
+	{
+		private readonly IStringLocalizer<Account_RegisterUserModel> t = _t;
+		public string Title => t["Register"];
+		public List<string> Lines { get; set; } = [];
+
+		public void OnGet()
+		{
+			Lines.Add(t["Enter the email address, which you want to register into the application"]);
+			Lines.Add($"{t["If you are already registered, you can"]} <a href='/Login'> {t["login-"]} </a>");
+			Lines.Add($"{t["If you forgot the password"]} <a href='/ForgottenPassword'> {t["click here"]}</a>");
+		}
+	}
 }
