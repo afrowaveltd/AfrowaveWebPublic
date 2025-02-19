2edbab0b55975acf76da7552a4508896fa57f6ef
Bohuslav Pařenica <sparenica@outlook.com>
2025-02-19 15:55:37 +0100

    email manager - added template emails and group templated emails

--- Id/Services/EmailManager.cs
+++ Id/Services/EmailManager.cs
@@ -83,6 +83,117 @@ namespace Id.Services
 			return await SendEmailAsync(message, smtpSettings);
 		}
 
+		public async Task<EmailResult> SendEmailAsync(string targetEmail, string subject, string body)
+		{
+			return await SendEmailAsync(targetEmail, subject, body, _authenticatorId);
+		}
+
+		public async Task<EmailResult> SendEmailFromTemplateAsync(string targetEmail, string templateName, object model, string applicationId)
+		{
+			ApplicationSmtpSettings smtpSettings = await _applicationManager.GetApplicationSmtpSettingsAsync(applicationId);
+			if(smtpSettings == null)
+			{
+				_logger.LogError("SMTP settings not found for application {applicationId}", applicationId);
+				return new EmailResult
+				{
+					TargetEmail = targetEmail,
+					Subject = templateName,
+					Success = false,
+					ErrorMessage = _t["SMTP settings not found"]
+				};
+			}
+			if(string.IsNullOrEmpty(targetEmail))
+			{
+				_logger.LogError("Target email is empty");
+				return new EmailResult
+				{
+					TargetEmail = targetEmail,
+					Subject = templateName,
+					Success = false,
+					ErrorMessage = _t["Target email is empty"]
+				};
+			}
+			if(string.IsNullOrEmpty(templateName))
+			{
+				_logger.LogError("Template name is empty");
+				return new EmailResult
+				{
+					TargetEmail = targetEmail,
+					Subject = templateName,
+					Success = false,
+					ErrorMessage = _t["Template name is empty"]
+				};
+			}
+			string body = await _razorEngine.CompileRenderAsync($"Templates.{templateName}", model);
+			string subject = model.GetType().GetProperty("Subject")?.GetValue(model)?.ToString() ?? _t["Notification"];
+			return await SendEmailAsync(targetEmail, subject, body, applicationId);
+		}
+
+		public async Task<EmailResult> SendEmailFromTemplateAsync(string targetEmail, string templateName, object model)
+		{
+			return await SendEmailFromTemplateAsync(targetEmail, templateName, model, _authenticatorId);
+		}
+
+		public async Task<SendEmailsResult> SendGroupEmailFromTemplateAsync(List<string> targetEmails, string templateName, object model, string applicationId)
+		{
+			SendEmailsResult result = new();
+			result.Subject = model.GetType().GetProperty("Subject")?.GetValue(model)?.ToString() ?? _t["Notification"];
+			// do checks if address list is not empty, and in each loop check if the recepient address is valid
+			if(targetEmails != null && targetEmails.Count > 0)
+			{
+				foreach(string recepient in targetEmails)
+				{
+					if(!string.IsNullOrEmpty(recepient) && new EmailAddressAttribute().IsValid(recepient))
+					{
+						EmailResult emailResult = await SendEmailFromTemplateAsync(recepient, templateName, model, applicationId);
+						result.Results.Add(emailResult);
+					}
+					else
+					{
+						_logger.LogError("Invalid email address: {recepient}", recepient);
+						result.Results.Add(new EmailResult
+						{
+							TargetEmail = recepient,
+							Subject = model.GetType().GetProperty("Subject")?.GetValue(model)?.ToString() ?? _t["Notification"],
+							Success = false,
+							ErrorMessage = _t["Invalid email address"]
+						});
+					}
+				}
+				return result;
+			}
+			else
+				if(targetEmails == null)
+			{
+				_logger.LogError("Target email list is null");
+				result.Results.Add(new EmailResult
+				{
+					TargetEmail = "",
+					Subject = model.GetType().GetProperty("Subject")?.GetValue(model)?.ToString() ?? _t["Notification"],
+					Success = false,
+					ErrorMessage = _t["Target email list is null"]
+				});
+				return result;
+			}
+			else
+			{
+				_logger.LogError("Target email list is empty");
+				result.Results.Add(new EmailResult
+				{
+					TargetEmail = "",
+					Subject = model.GetType().GetProperty("Subject")?.GetValue(model)?.ToString() ?? _t["Notification"],
+					Success = false,
+					ErrorMessage = _t["Target email list is empty"]
+				});
+				return result;
+			}
+		}
+
+		public async Task<SendEmailsResult> SendGroupEmailFromTemplateAsync(List<string> targetEmails, string templateName, object model)
+		{
+			return await SendGroupEmailFromTemplateAsync(targetEmails, templateName, model, _authenticatorId);
+		}
+
 		public async Task<SendEmailsResult> SendGroupEmailAsync(List<string> targetEmails, string subject, string body, string applicationId)
 		{
 			SendEmailsResult result = new();
@@ -138,6 +249,11 @@ namespace Id.Services
 			}
 		}
 
+		public async Task<SendEmailsResult> SendGroupEmailAsync(List<string> targetEmails, string subject, string body)
+		{
+			return await SendGroupEmailAsync(targetEmails, subject, body, _authenticatorId);
+		}
+
 		// private methods
 		private async Task<EmailResult> SendEmailAsync(MimeMessage message, ApplicationSmtpSettings settings)
 		{
@@ -152,7 +268,7 @@ namespace Id.Services
 				{
 					await client.AuthenticateAsync(settings.Username, settings.Password);
 				}
-				await client.SendAsync(message);
+				_ = await client.SendAsync(message);
 				await client.DisconnectAsync(true);
 				result.Success = true;
 			}
